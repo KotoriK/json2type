974f73a5c6d04711d351363a86a00c43b76686a9
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-21 14:57:58 +0800

    fix: cli

--- cli.ts
+++ cli.ts
@@ -1,9 +1,9 @@
 import { readFile, writeFile } from "fs/promises";
 import { parseToTypes } from './index'
 readFile('./input.txt', 'utf-8').then(str =>
-    parseToTypes(JSON.parse(str))
-).then((str) =>
+    parseToTypes(str)
+).then(str =>
     writeFile('./output.ts', str, 'utf-8')
-).finally(() => {
+).then(() => {
     console.log('Job done.')
 })
