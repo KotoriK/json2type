14369ab169edcad1ba981e0f4663db4dbfcfa225
KotoriK <52659125+KotoriK@users.noreply.github.com>
2024-12-15 20:19:50 +0800

    chore: dist

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,265 @@
-var e=require("change-case"),r=require("diff");function t(e,r){(null==r||r>e.length)&&(r=e.length);for(var t=0,a=new Array(r);t<r;t++)a[t]=e[t];return a}function a(e,r){var a="undefined"!=typeof Symbol&&e[Symbol.iterator]||e["@@iterator"];if(a)return(a=a.call(e)).next.bind(a);if(Array.isArray(e)||(a=function(e,r){if(e){if("string"==typeof e)return t(e,r);var a=Object.prototype.toString.call(e).slice(8,-1);return"Object"===a&&e.constructor&&(a=e.constructor.name),"Map"===a||"Set"===a?Array.from(e):"Arguments"===a||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(a)?t(e,r):void 0}}(e))||r&&e&&"number"==typeof e.length){a&&(e=a);var n=0;return function(){return n>=e.length?{done:!0}:{done:!1,value:e[n++]}}}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self&&self;var n,i,o=(n=function(e,r){e.exports=function(){var e=[],r=[],t={},a={},n={};function i(e){return"string"==typeof e?new RegExp("^"+e+"$","i"):e}function o(e,r){return e===r?r:e===e.toLowerCase()?r.toLowerCase():e===e.toUpperCase()?r.toUpperCase():e[0]===e[0].toUpperCase()?r.charAt(0).toUpperCase()+r.substr(1).toLowerCase():r.toLowerCase()}function s(e,r){return e.replace(/\$(\d{1,2})/g,function(e,t){return r[t]||""})}function u(e,r){return e.replace(r[0],function(t,a){var n=s(r[1],arguments);return o(""===t?e[a-1]:t,n)})}function c(e,r,a){if(!e.length||t.hasOwnProperty(e))return r;for(var n=a.length;n--;){var i=a[n];if(i[0].test(r))return u(r,i)}return r}function l(e,r,t){return function(a){var n=a.toLowerCase();return r.hasOwnProperty(n)?o(a,n):e.hasOwnProperty(n)?o(a,e[n]):c(n,a,t)}}function h(e,r,t,a){return function(a){var n=a.toLowerCase();return!!r.hasOwnProperty(n)||!e.hasOwnProperty(n)&&c(n,n,t)===n}}function f(e,r,t){return(t?r+" ":"")+(1===r?f.singular(e):f.plural(e))}return f.plural=l(n,a,e),f.isPlural=h(n,a,e),f.singular=l(a,n,r),f.isSingular=h(a,n,r),f.addPluralRule=function(r,t){e.push([i(r),t])},f.addSingularRule=function(e,t){r.push([i(e),t])},f.addUncountableRule=function(e){"string"!=typeof e?(f.addPluralRule(e,"$0"),f.addSingularRule(e,"$0")):t[e.toLowerCase()]=!0},f.addIrregularRule=function(e,r){r=r.toLowerCase(),e=e.toLowerCase(),n[e]=r,a[r]=e},[["I","we"],["me","us"],["he","they"],["she","they"],["them","them"],["myself","ourselves"],["yourself","yourselves"],["itself","themselves"],["herself","themselves"],["himself","themselves"],["themself","themselves"],["is","are"],["was","were"],["has","have"],["this","these"],["that","those"],["echo","echoes"],["dingo","dingoes"],["volcano","volcanoes"],["tornado","tornadoes"],["torpedo","torpedoes"],["genus","genera"],["viscus","viscera"],["stigma","stigmata"],["stoma","stomata"],["dogma","dogmata"],["lemma","lemmata"],["schema","schemata"],["anathema","anathemata"],["ox","oxen"],["axe","axes"],["die","dice"],["yes","yeses"],["foot","feet"],["eave","eaves"],["goose","geese"],["tooth","teeth"],["quiz","quizzes"],["human","humans"],["proof","proofs"],["carve","carves"],["valve","valves"],["looey","looies"],["thief","thieves"],["groove","grooves"],["pickaxe","pickaxes"],["passerby","passersby"]].forEach(function(e){return f.addIrregularRule(e[0],e[1])}),[[/s?$/i,"s"],[/[^\u0000-\u007F]$/i,"$0"],[/([^aeiou]ese)$/i,"$1"],[/(ax|test)is$/i,"$1es"],[/(alias|[^aou]us|t[lm]as|gas|ris)$/i,"$1es"],[/(e[mn]u)s?$/i,"$1s"],[/([^l]ias|[aeiou]las|[ejzr]as|[iu]am)$/i,"$1"],[/(alumn|syllab|vir|radi|nucle|fung|cact|stimul|termin|bacill|foc|uter|loc|strat)(?:us|i)$/i,"$1i"],[/(alumn|alg|vertebr)(?:a|ae)$/i,"$1ae"],[/(seraph|cherub)(?:im)?$/i,"$1im"],[/(her|at|gr)o$/i,"$1oes"],[/(agend|addend|millenni|dat|extrem|bacteri|desiderat|strat|candelabr|errat|ov|symposi|curricul|automat|quor)(?:a|um)$/i,"$1a"],[/(apheli|hyperbat|periheli|asyndet|noumen|phenomen|criteri|organ|prolegomen|hedr|automat)(?:a|on)$/i,"$1a"],[/sis$/i,"ses"],[/(?:(kni|wi|li)fe|(ar|l|ea|eo|oa|hoo)f)$/i,"$1$2ves"],[/([^aeiouy]|qu)y$/i,"$1ies"],[/([^ch][ieo][ln])ey$/i,"$1ies"],[/(x|ch|ss|sh|zz)$/i,"$1es"],[/(matr|cod|mur|sil|vert|ind|append)(?:ix|ex)$/i,"$1ices"],[/\b((?:tit)?m|l)(?:ice|ouse)$/i,"$1ice"],[/(pe)(?:rson|ople)$/i,"$1ople"],[/(child)(?:ren)?$/i,"$1ren"],[/eaux$/i,"$0"],[/m[ae]n$/i,"men"],["thou","you"]].forEach(function(e){return f.addPluralRule(e[0],e[1])}),[[/s$/i,""],[/(ss)$/i,"$1"],[/(wi|kni|(?:after|half|high|low|mid|non|night|[^\w]|^)li)ves$/i,"$1fe"],[/(ar|(?:wo|[ae])l|[eo][ao])ves$/i,"$1f"],[/ies$/i,"y"],[/\b([pl]|zomb|(?:neck|cross)?t|coll|faer|food|gen|goon|group|lass|talk|goal|cut)ies$/i,"$1ie"],[/\b(mon|smil)ies$/i,"$1ey"],[/\b((?:tit)?m|l)ice$/i,"$1ouse"],[/(seraph|cherub)im$/i,"$1"],[/(x|ch|ss|sh|zz|tto|go|cho|alias|[^aou]us|t[lm]as|gas|(?:her|at|gr)o|[aeiou]ris)(?:es)?$/i,"$1"],[/(analy|diagno|parenthe|progno|synop|the|empha|cri|ne)(?:sis|ses)$/i,"$1sis"],[/(movie|twelve|abuse|e[mn]u)s$/i,"$1"],[/(test)(?:is|es)$/i,"$1is"],[/(alumn|syllab|vir|radi|nucle|fung|cact|stimul|termin|bacill|foc|uter|loc|strat)(?:us|i)$/i,"$1us"],[/(agend|addend|millenni|dat|extrem|bacteri|desiderat|strat|candelabr|errat|ov|symposi|curricul|quor)a$/i,"$1um"],[/(apheli|hyperbat|periheli|asyndet|noumen|phenomen|criteri|organ|prolegomen|hedr|automat)a$/i,"$1on"],[/(alumn|alg|vertebr)ae$/i,"$1a"],[/(cod|mur|sil|vert|ind)ices$/i,"$1ex"],[/(matr|append)ices$/i,"$1ix"],[/(pe)(rson|ople)$/i,"$1rson"],[/(child)ren$/i,"$1"],[/(eau)x?$/i,"$1"],[/men$/i,"man"]].forEach(function(e){return f.addSingularRule(e[0],e[1])}),["adulthood","advice","agenda","aid","aircraft","alcohol","ammo","analytics","anime","athletics","audio","bison","blood","bream","buffalo","butter","carp","cash","chassis","chess","clothing","cod","commerce","cooperation","corps","debris","diabetes","digestion","elk","energy","equipment","excretion","expertise","firmware","flounder","fun","gallows","garbage","graffiti","hardware","headquarters","health","herpes","highjinks","homework","housework","information","jeans","justice","kudos","labour","literature","machinery","mackerel","mail","media","mews","moose","music","mud","manga","news","only","personnel","pike","plankton","pliers","police","pollution","premises","rain","research","rice","salmon","scissors","series","sewage","shambles","shrimp","software","species","staff","swine","tennis","traffic","transportation","trout","tuna","wealth","welfare","whiting","wildebeest","wildlife","you",/pok[eé]mon$/i,/[^aeiou]ese$/i,/deer$/i,/fish$/i,/measles$/i,/o[iu]s$/i,/pox$/i,/sheep$/i].forEach(f.addUncountableRule),f}()},n(i={exports:{}}),i.exports),s=/*#__PURE__*/function(){function r(){this._cache={},this._cache_r={},this._unnameCount=0}var t=r.prototype;return t._printCache=function(){var e=Object.entries(this._cache_r);return e.length>0?e.map(function(e){return"interface "+e[0]+e[1]}).join("\n"):""},t.parseToTypes=function(e,r){if(void 0===r&&(r="DefaultInterface"),"object"!=typeof e)throw TypeError('param "obj" must be an object, but got '+typeof e);return"interface "+r+this._parseObjectToTypes(e)+"\n"+this._printCache()},t._parseObjectToTypes=function(e){var r=this;return"{\n"+Object.entries(e).sort(d).map(function(e){var t,a=e[1],n=(t=e[0]).match(/^\d/)||t.match(/[\u0000-#%-/:-@[-^`{-\u007f]/)?'"'+t+'"':t;return n+":"+r._typeof(a,n)}).join("\n")+"\n}"},t._printArrayType=function(e,r){for(var t,n,i=this,s=new Set,c=a(e);!(n=c()).done;)s.add(u(n.value));if(1==s.size&&s.has("Record<string,any>")){s.clear();for(var l,f=a(e);!(l=f()).done;)s.add(this._checkThenParseObject(l.value,r&&o.singular(r)));var p=Array.from(s);if(1==s.size)t=p[0];else{for(var m=p.map(function(e){return i._cache_r[e]}),d=m[0],$=p[0],y=1;y<m.length;y++){var g=h(d,m[y]);if(!g)return"Array<"+(t=p.join(" | "))+">";d=g,$=$.concat(p[y])}for(var v=0;v<m.length;v++)this._cache[m[v]]=$,delete this._cache_r[p[v]];this._cache_r[$]=d,t=$}}else t=0===e.length?"unknown":Array.from(s).join(" | ");return"Array<"+t+">"},t._checkThenParseObject=function(r,t){if(r instanceof Array)return this._printArrayType(r,t);if(null!=r){if(t){var a=this._tryParseIdMap(r,t);if(a)return a}var n=this._parseObjectToTypes(r);if(n.match(/{\s*}/))return n;var i=this._cache[n];if(i)return i;for(var o,s=t?e.pascalCase(t.match(/^["']\d/)?"I"+t:t):this._defaultName();o=this._cache_r[s];){var u=l(o,n);if(u)return this._cache[u]=s,this._cache[o]=s,this._cache[n]=s,this._cache_r[s]=u,s;s=s.concat("_")}return this._cache_r[s]=n,this._cache[n]=s,s}return"null"},t._tryParseIdMap=function(e,r){var t=this,a=Array.from(Object.keys(e));if(a.length>0&&a.every(function(e){return e.match(/^\d+$/)})){var n=o.singular(r);return"{[id:number]:"+Array.from(new Set(Object.values(e).map(function(e){return t._typeof(e,n)}))).join("|")+"}"}},t._typeof=function(e,r){var t=typeof e;switch(t){case"object":return this._checkThenParseObject(e,r);case"string":case"number":case"boolean":return t;default:throw t+" isn't support yet."}},t._defaultName=function(){return"I"+this._unnameCount++},r}();function u(e){var r=typeof e;switch(r){case"object":return null===e?"undefined":"Record<string,any>";case"string":case"number":case"boolean":return r;default:throw r+" isn't support yet."}}var c=function(e){return e.startsWith("{")&&e.endsWith("}")};function l(e,r){if(c(e)&&c(r)){var t=f(e,r),a=t[0],n=t[1],i=t[2];if(a.length>=n.length+i.length)return p(a,n,i)}}var h=function(e,r){return p.apply(void 0,f(e,r))};function f(e,t){for(var n,i=[],o=[],s=[],u=a(r.diffLines(e.replaceAll(/^{|}/gm,""),t.replaceAll(/^{|}/gm,"")));!(n=u()).done;){var c=n.value;c.added?o.push.apply(o,m(c.value)):c.removed?s.push.apply(s,m(c.value)):i.push.apply(i,m(c.value))}return[i,o,s]}function p(e,r,t){for(var n,i=Object.fromEntries(t),o=a(r);!(n=o()).done;){var s=n.value,u=i[s[0]];if(u&&u!=s[1])return}return"{\n"+[].concat([].concat(r,t).sort(d).map(function(e){return e[0]+"?:"+e[1]}),e.map(function(e){return e[0]+":"+e[1]})).join("\n")+"\n}"}var m=function(e){return e.split("\n").filter(function(e){return""!=e}).map(function(e){return e.split(":")})};function d(e,r){for(var t,a=e[0],n=r[0],i=Math.min(a.length,n.length),o=0;o<i;o++)if(0!=(t=a.charCodeAt(o)-n.charCodeAt(o)))return t;return a.length-n.length}exports.Json2Type=s,exports.parseToTypes=function(e,r){return void 0===r&&(r="DefaultInterface"),(new s).parseToTypes(JSON.parse(e),r)};
-//# sourceMappingURL=index.js.map
+import { pascalCase as b } from "change-case";
+import { diffLines as j } from "diff";
+import v from "pluralize";
+/**
+ * json2type
+ * @author KotoriK
+ * @date 2021.6
+ * @license MIT
+ */
+const p = v.singular;
+class O {
+  /**
+   * 结构信息->名字
+   */
+  _cache = {};
+  /**
+   * 名字->结构信息
+   */
+  _cache_r = {};
+  _unnameCount = 0;
+  _printCache() {
+    const e = Object.entries(this._cache_r);
+    return e.length > 0 ? e.map(([n, t]) => `interface ${n}${t}`).join(`
+`) : "";
+  }
+  /**
+   * 
+   * @param {Record<string,any>} obj 
+   * @param {string} name
+   */
+  parseToTypes(e, n = "DefaultInterface") {
+    if (typeof e != "object") throw TypeError('param "obj" must be an object, but got ' + typeof e);
+    return `interface ${n}${this._parseObjectToTypes(e)}
+${this._printCache()}`;
+  }
+  /**
+   * @private
+   * @param obj 
+   * @returns {string}
+   */
+  _parseObjectToTypes(e) {
+    return `{
+` + Object.entries(e).sort(T).map(
+      ([n, t]) => {
+        const i = w(n);
+        return `${i}:${this._typeof(t, i)}`;
+      }
+    ).join(`
+`) + `
+}`;
+  }
+  /**
+   * 推断数组内部元素的类型
+   * @private
+   * @param {Array} arr 
+   * @param {string} key 数组的字段名，用于命名内部元素
+   * @returns {string}
+   */
+  _printArrayType(e, n) {
+    const t = /* @__PURE__ */ new Set();
+    let i;
+    for (const s of e)
+      t.add($(s));
+    if (t.size == 1 && t.has("Record<string,any>")) {
+      t.clear();
+      for (const c of e)
+        t.add(this._checkThenParseObject(c, n && p(n)));
+      const s = Array.from(t);
+      if (t.size == 1)
+        i = s[0];
+      else {
+        const c = s.map((a) => this._cache_r[a]);
+        let o = c[0], u = s[0];
+        for (let a = 1; a < c.length; a++) {
+          const l = c[a], f = A(o, l);
+          if (f)
+            o = f, u = u.concat(s[a]);
+          else
+            return i = s.join(" | "), `Array<${i}>`;
+        }
+        for (let a = 0; a < c.length; a++)
+          this._cache[c[a]] = u, delete this._cache_r[s[a]];
+        this._cache_r[u] = o, i = u;
+      }
+    } else e.length === 0 ? i = "unknown" : i = _(t, " | ");
+    return `Array<${i}>`;
+  }
+  /**
+   * @private
+   * @param foo 
+   * @param key 
+   * @returns 
+   */
+  _checkThenParseObject(e, n) {
+    if (e instanceof Array)
+      return this._printArrayType(e, n);
+    if (e != null) {
+      if (n) {
+        const s = this._tryParseIdMap(e, n);
+        if (s) return s;
+      }
+      const t = this._parseObjectToTypes(
+        e
+        /* key */
+      );
+      if (t.match(/{\s*}/)) return t;
+      const i = this._cache[t];
+      if (i)
+        return i;
+      {
+        let s = n ? b(n.match(/^["']\d/) ? "I" + n : n) : this._defaultName(), c;
+        for (; c = this._cache_r[s]; ) {
+          const o = S(c, t);
+          if (o)
+            return this._cache[o] = s, this._cache[c] = s, this._cache[t] = s, this._cache_r[s] = o, s;
+          s = s.concat("_");
+        }
+        return this._cache_r[s] = t, this._cache[t] = s, s;
+      }
+    }
+    return "null";
+  }
+  /**
+   * try to parse Index Signatures Object
+   * @seealso https://www.typescriptlang.org/docs/handbook/2/objects.html#index-signatures
+   * @param foo 
+   * @param key 
+   * @returns 
+   */
+  _tryParseIdMap(e, n) {
+    const t = Object.keys(e);
+    if (t.length > 0 && t.every((i) => i.match(/^\d+$/))) {
+      const i = p(n);
+      return `{[id:number]:${_(
+        new Set(
+          Object.values(e).map((s) => this._typeof(s, i))
+        ),
+        "|"
+      )}}`;
+    }
+  }
+  /**
+   * @private
+   * @param foo 
+   * @param {string | undefined} key
+   * @returns {string}
+   */
+  _typeof(e, n) {
+    let t = typeof e;
+    switch (t) {
+      case "object":
+        return this._checkThenParseObject(e, n);
+      /**按原样 */
+      case "string":
+      case "number":
+      case "boolean":
+        return t;
+      default:
+        throw t + " isn't support yet.";
+    }
+  }
+  /**
+   * @private
+   * @returns {string}
+   */
+  _defaultName() {
+    return "I" + this._unnameCount++;
+  }
+}
+function $(r) {
+  let e = typeof r;
+  switch (e) {
+    case "object":
+      return r === null ? "undefined" : "Record<string,any>";
+    /**按原样 */
+    case "string":
+    case "number":
+    case "boolean":
+      return e;
+    default:
+      throw e + " isn't support yet.";
+  }
+}
+function w(r) {
+  return r.match(/^\d/) ? `"${r}"` : r.match(/[\u0000-#%-/:-@[-^`{-\u007f]/) ? `"${r}"` : r;
+}
+const d = (r) => r.startsWith("{") && r.endsWith("}");
+function S(r, e) {
+  if (d(r) && d(e)) {
+    const [n, t, i] = y(r, e);
+    return g(n, t, i);
+  }
+}
+const A = (r, e) => g(...y(r, e));
+function y(r, e) {
+  const n = j(r.replaceAll(/^{|}/mg, ""), e.replaceAll(/^{|}/mg, "")), t = [], i = [], s = [];
+  for (const c of n)
+    c.added ? i.push(...h(c.value)) : c.removed ? s.push(...h(c.value)) : t.push(...h(c.value));
+  return [t, i, s];
+}
+function m(r) {
+  switch (r) {
+    case "string":
+    case "boolean":
+    case "number":
+      return !0;
+    default:
+      return !1;
+  }
+}
+function g(r, e, n) {
+  const t = Object.fromEntries(n), i = [], s = [];
+  for (const c of e) {
+    const [o, u] = c;
+    let a = o, l = t[a] || t[a = o + "?"];
+    if (l) {
+      if (l !== u) {
+        if (m(l) && m(u))
+          c[1] = l + "|" + u;
+        else if (l === "null" || u === "null")
+          c[0] += "?";
+        else
+          return;
+        i.push(c), delete t[a];
+      }
+    } else
+      s.push(c);
+  }
+  for (const c of Object.entries(t))
+    s.push(c);
+  return `{
+` + Array.from(
+    C(r, i, s.map(([c, o]) => [c.endsWith("?") ? c : c + "?", o]))
+  ).sort(T).map(([c, o]) => `${c}:${o}`).join(`
+`) + `
+}`;
+}
+const h = (r) => r.split(`
+`).filter((e) => e).map((e) => e.split(":"));
+function T([r], [e]) {
+  const n = Math.min(r.length, e.length);
+  let t;
+  for (let i = 0; i < n; i++)
+    if (t = r.charCodeAt(i) - e.charCodeAt(i), t !== 0)
+      return t;
+  return r.length - e.length;
+}
+function _(r, e) {
+  let n = "";
+  for (const t of r)
+    n += t + e;
+  return n.slice(0, -e.length);
+}
+function* C(...r) {
+  for (const e of r)
+    for (const n of e)
+      yield n;
+}
+function N(r, e = "DefaultInterface") {
+  return new O().parseToTypes(JSON.parse(r), e);
+}
+export {
+  O as Json2Type,
+  N as parseToTypes
+};
