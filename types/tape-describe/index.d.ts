756a4ddcb036f78d949d65e527164fe45ca0dae7
KotoriK <52659125+KotoriK@users.noreply.github.com>
2024-12-15 20:22:26 +0800

    remove

--- types/tape-describe/index.d.ts
+++
@@ -1,5 +0,0 @@
-import tape from "tape";
-
-function describe(blockName: string): typeof tape
-function describe(blockName: string, callback: (arg_test: typeof tape) => void): void
-export default describe
